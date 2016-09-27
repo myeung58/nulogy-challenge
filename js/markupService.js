483918e22655c83da083611b8f1160497c600945
myeung58 <myeung58@gmail.com>
2016-09-27 14:36:05 -0400

    base markup

--- js/markupService.js
+++ js/markupService.js
@@ -3,8 +3,19 @@ var MarkupService = function(basePrice, labor, material) {
       typeof labor !== 'number' ||
       typeof material !== 'string') { return; }
 
+  var markup = {
+    BASE: 0.05,
+    LABOR: 0.012,
+    DRUGS: 0.075,
+    FOOD: 0.13,
+    ELECTRONICS: 0.02
+  };
+
   return {
-    baseMarkup: function() {},
+    baseMarkup: function() {
+      var baseMarkup = basePrice * (1 + markup.BASE) - basePrice;
+      return parseFloat(baseMarkup.toFixed(2));
+    },
     laborMarkup: function() {},
     categoryMarkup: function() {},
     totalMarkup: function() {}
--- test/MarkupServiceTests.js
+++ test/MarkupServiceTests.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 var MarkupService = require('../js/MarkupService');
 
 describe('MarkupService', function() {
-  it('returns nothing if no param passed in', function() {
+  it('returns empty object if no param passed in', function() {
     var service = new MarkupService();
 
     expect(service).to.be.an('object');
@@ -16,5 +16,23 @@ describe('MarkupService', function() {
 
     expect(service).to.be.an('object');
     expect(service).to.have.keys('baseMarkup', 'laborMarkup', 'categoryMarkup', 'totalMarkup');
+  });
+
+  describe('.baseMarkup', function() {
+    it('returns 0 if basePrice is 0', function() {
+      var service = new MarkupService(0, 1, '');
+
+      expect(service.baseMarkup()).to.equal(0);
+    });
+
+    it('returns correct base markup', function() {
+      var service = new MarkupService(1000, 0, ''),
+        service2 = new MarkupService(1, 0, ''),
+        service3 = new MarkupService(123.45, 0, '');
+
+      expect(service.baseMarkup()).to.equal(50);
+      expect(service2.baseMarkup()).to.equal(0.05);
+      expect(service3.baseMarkup()).to.equal(6.17);
+    });
   });
 });
