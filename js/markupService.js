70d2418111691b90b63076ce2a54f6194f3665e7
myeung58 <myeung58@gmail.com>
2016-09-27 14:29:33 -0400

    return object with functions

--- js/markupService.js
+++ js/markupService.js
@@ -1,5 +1,14 @@
 var MarkupService = function(basePrice, labor, material) {
-  if (!basePrice, !labor, !material) { return; }
+  if (typeof basePrice !== 'number' ||
+      typeof labor !== 'number' ||
+      typeof material !== 'string') { return; }
+
+  return {
+    baseMarkup: function() {},
+    laborMarkup: function() {},
+    categoryMarkup: function() {},
+    totalMarkup: function() {}
+  };
 };
 
 module.exports = MarkupService;
--- test/MarkupServiceTests.js
+++ test/MarkupServiceTests.js
@@ -10,4 +10,11 @@ describe('MarkupService', function() {
     expect(service).to.be.an('object');
     expect(service).to.be.empty;
   });
+
+  it('returns an object with correct functions if valid params are passed in', function() {
+    var service = new MarkupService(1000, 1, '');
+
+    expect(service).to.be.an('object');
+    expect(service).to.have.keys('baseMarkup', 'laborMarkup', 'categoryMarkup', 'totalMarkup');
+  });
 });
